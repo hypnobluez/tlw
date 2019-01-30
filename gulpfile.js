2a06cd86a46b27400c6aac0c573066e314b06673
hypnobluez <hypnobluez@gmail.com>
2019-01-30 20:01:21 +1100

    gulp restructure

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,53 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var postcss = require('gulp-postcss');
 var tailwindcss = require('tailwindcss');
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 
-gulp.task('sass', function () {
-    return gulp.src('./styles/scss/*.scss')
+gulp.task('clean', function () {
+    return del(['./src/css/temp', './src/css/temp.css']);
+});
+
+gulp.task('sass-one', function () {
+    return gulp.src('./src/scss/*.scss')
         .pipe(sass())
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('./styles/css'));
+        .pipe(gulp.dest('./src/css/temp'));
+});
+
+gulp.task('css:minify', function cssMinify() {
+    return gulp.src('./src/css/temp.css')
+        .pipe(cleanCSS())
+        .pipe(rename('theme.css'))
+        .pipe(gulp.dest('./src/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('tailwind', function () {
-    return gulp.src('./styles/css/main.css')
+    return gulp.src('./src/css/temp/temp.css')
         .pipe(postcss([
             tailwindcss('./tailwind.js'),
             autoprefixer,
         ]))
-        .pipe(gulp.dest('./styles/public/css/'))
+        .pipe(gulp.dest('./src/css/'))
 });
-gulp.task('serve', gulp.series('sass', 'tailwind', function () {
+gulp.task('build-css', gulp.series('sass-one', 'tailwind', 'css:minify', 'clean'));
+
+gulp.task('serve', gulp.series('build-css', function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./src"
         }
     });
 
-    gulp.watch("./styles/scss/*.scss", gulp.series('sass'));
-    gulp.watch("./styles/css/*.css", gulp.series('tailwind', browserSync.reload));
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("src/scss/**/*.scss", gulp.series('build-css', browserSync.reload));
+    gulp.watch("src/**/*.html").on('change', browserSync.reload);
 }));
+
 
 
 
